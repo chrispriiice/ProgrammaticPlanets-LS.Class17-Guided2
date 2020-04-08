bbcf27407c17d8cffd77bb5562859e02040cbcce
Chris Price <christopher.r.price@gmail.com>
2020-04-08 00:15:51 -0400

    Finished project

--- ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
+++ ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
@@ -19,10 +19,80 @@ class PlanetCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubviews()
+    }
+    
     private func updateViews() {
         guard let planet = planet else { return }
         imageView.image = planet.image
         nameLabel.text = planet.name
     }
     
+    private func setUpSubviews() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        // Y
+        NSLayoutConstraint(
+            item: imageView,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: self,
+            attribute: .top,
+            multiplier: 1,
+            constant: 4).isActive = true
+        
+        // X
+        NSLayoutConstraint(
+            item: imageView,
+            attribute: .leading,
+            relatedBy: .equal,
+            toItem: self,
+            attribute: .leading,
+            multiplier: 1,
+            constant: 4).isActive = true
+        
+        // Width
+        NSLayoutConstraint(
+            item: imageView,
+            attribute: .trailing,
+            relatedBy: .equal,
+            toItem: self,
+            attribute: .trailing,
+            multiplier: 1,
+            constant: -4).isActive = true
+        
+        // Height
+        NSLayoutConstraint(
+            item: imageView,
+            attribute: .height,
+            relatedBy: .equal,
+            toItem: imageView,
+            attribute: .width,
+            multiplier: 1,
+            constant: 0).isActive = true
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textAlignment = .center
+        addSubview(nameLabel)
+        
+        // Y
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4).isActive = true
+        
+        // X
+        nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+        
+        // Width
+        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2).isActive = true
+        
+        // Height is intrinsic for labels. 
+    }
 }
